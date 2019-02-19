9a7dad753b03220611bf654b725742383fc7b221
Mc Clynrey Arboleda (Mac) <mcclynrey@gmail.com>
2019-02-20 00:07:58 +0800

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Mc Clynrey Arboleda on 20/02/2019.
+//  Copyright © 2019 Mc Clynrey Arboleda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
